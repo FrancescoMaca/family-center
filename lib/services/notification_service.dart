2cf06f84617f522a7ffafe3e4b3d33077b545470
Francesco Macaluso <francescomaca93@gmail.com>
2024-12-17 20:19:44 +0100

    Added test comments
    Added test comments for git patch

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -3,10 +3,10 @@
 class NotificationService {
 
   Future<void> sendNotification(/* Params */) async {
-    
+    // Added this changes for git test 1
   }
 
-  Future<void> markNotification() async {
-
+  Future<void> markNotification(/* Params */) async {
+    // Added this second change for git test 2
   }
 }
